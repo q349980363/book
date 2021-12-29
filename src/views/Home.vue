fe315fcb51121b31c8c3cecf6c3e1ced93214da5
q349980363 <759323585@qq.com>
2021-12-29 19:10:43 +0800

    初始化
    Co-authored-by: lingy.io@qq.com <lingy.io@qq.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <input type="text" class="inp" />
+    <p class="book-word">
+      <span>书籍</span>
+    </p>
+       <div class="book">
+       <div class="book"></div>
+       <p>书名</p>
+       </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="stylus">
+.inp
+  width 80%
+  height 30px
+</style>
