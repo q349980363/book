a3d380c55ccd2cf94885ff12d813f9da00647377
q349980363 <759323585@qq.com>
2021-12-29 19:13:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="stylus">
@@ -11,7 +11,13 @@
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
   color #2c3e50
-  margin-top 60px
+  background-color: rgb(255,0,0)
+
+html, body ,#app{
+  width 100%
+  height 100%
+  margin 0
+  padding 0
+}
 </style>
